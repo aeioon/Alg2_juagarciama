b0db21c505d97ef412537d373ecf5a4de2cfdc04
SGman98 <bastiangarnica@gmail.com>
2021-11-29 15:32:14 -0500

    Crear archivo biseccion
    Archivo calculo biseccion de bolzano usando python
    
    TODO:
    - README con documentacion del funcionamiento de la biseccion de bolzano
    
    Co-authored-by: eachaves <eachaves@users.noreply.github.com>
    Co-authored-by: sarahcoral <sarahcoral@users.noreply.github.com>

---
+++ lab3/biseccion.py
@@ -0,0 +1,48 @@
+# Metodo de Bolzano para encontrar raizes de function
+
+def biseccion (function, a, b, ite = 0, max_ite = 100, tol = 0.001):
+    c = (a + b)/2 # Calcula punto medio
+    error = abs(b - a) # Calcula error
+
+    # Imprime una tabla con los valores de # de iteracion, a, b, error
+    print(f"{ite:^11}\t{a:>12.5f}\t{b:>12.5f}\t±{error:>12.5f}")
+
+    # si excede el numero maximo de iteraciones
+    if ite > max_ite:
+        return f"El metodo no converge en {max_ite} iteraciones"
+
+    # Si f(a) y f(b) son del mismo signo --- No existe raiz
+    if function(a)*function(b) > 0:
+        print(f"\nNo hay raiz en el intervalo [{a}, {b}] ... ampliando el intervalo")
+        # Amplia el intervalo en ambas direcciones la mitad de la diferencia
+        a, b = a - (b - a)/2, b + (b - a)/2
+        print(f"Intervalo: [{a}, {b}]\n")
+        return biseccion(function, a, b, ite + 1, max_ite, tol)
+
+    # Si la tolerancia es menor que el error
+    if error < tol:
+        return c, error # Retorna el punto medio y el error
+    elif function(a) * function(c) < 0: # Si la funcion en el punto medio es menor que cero 
+        return biseccion(function, a, c, ite + 1, max_ite, tol) # Llamada recursiva con el intervalo [a, c]
+    else:
+        return biseccion(function, c, b, ite + 1, max_ite, tol) # Llamada recursiva con el intervalo [c, b]
+
+def main():
+    # Declaracion de la funcion del modelo
+    # function = lambda x:-x**4 + 30*(x**3) + 15*(x**2) + 34*x + 540
+    # x0 = 0 # Inicio del intervalo
+    # x1 = 1000 # Fin del intervalo
+    function_str = input("Ingrese la funcion con notacion de python: \n\tf(x) = ")
+    function = lambda x: eval(function_str)
+    x0 = float(input("Ingrese el valor inicial del intervalo: \n\tx0 = "))
+    x1 = float(input("Ingrese el valor final del intervalo: \n\tx1 = "))
+
+    # Comienzo del intervalo
+    print(f"\nEvaluando f(x): {function_str} en el intervalo [{x0}, {x1}]\n")
+    print(f"{'Iteracion':^11}\t{'a':^12}\t{'b':^12}\t {'Error':^12}")
+    print(f"{'-'*11}\t{'-'*12}\t{'-'*12}\t {'-'*12}")
+    result = biseccion(function, x0, x1)
+    print(f"\nRaiz: {result[0]:.5f} ± {result[1]:.5f}")
+
+if __name__ == "__main__":
+    main()
